2ac799bea172fe2e7e1e2f5d4de72cee2a67df0a
angelorapana <angelorapana@icloud.com>
2025-12-13 09:59:00 +0100

    analisi esercizio

---
+++ script.js
@@ -0,0 +1,25 @@
+//Palidroma 
+// Chiedere all’utente di inserire una parola 
+// Creare una funzione per capire se la parola inserita è palindroma 
+// Parola palindroma: la parola e' uguale a se stesa con i caratteri invertiti 
+// Ad esempio: anna => anna; otto => otto; ottetto => ottetto;
+
+//Richiesta inserimento parola:
+let parola = parseInt(prompt("Inserisci una parola"));
+
+//Prendo la parola inserita e verifico se è uguale alla parola inserita ma invertita:
+
+let parolaReverse = parola.reverse();
+
+console.log(parolaReverse);
+
+
+
+
+
+//Se la parola risulta essere palindroma quindi TRUE:
+// console.log("La tua parola è palindroma")
+
+
+//Se la parola risulta NON essere palindroma quindi FALSE:
+// console.log("La tua parola NON è palindroma")
